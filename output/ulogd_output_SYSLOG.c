d42d0afeea20d83ae79bdb1e25fe08f9f3cd555e
Holger Eitzenberger <holger@eitzenberger.org>
2008-09-17 10:46:49 +0200

    SYSLOG: use new 'ptr' type to avoid warning
    Signed-off-by: Holger Eitzenberger <holger@eitzenberger.org>

--- output/ulogd_output_SYSLOG.c
+++ output/ulogd_output_SYSLOG.c
@@ -77,7 +77,7 @@ static int _output_syslog(struct ulogd_pluginstance *upi)
 
 	if (res[0].u.source->flags & ULOGD_RETF_VALID)
 		syslog(li->syslog_level | li->syslog_facility, "%s",
-				res[0].u.source->u.value.ptr);
+				res[0].u.source->u.value.str);
 
 	return 0;
 }
