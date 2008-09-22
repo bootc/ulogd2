dba91033a13903881756b919afbbf5265ccc30b7
Holger Eitzenberger <holger@eitzenberger.org>
2008-09-22 09:36:14 +0200

    BASE: make key handling more robust
    Signed-off-by: Holger Eitzenberger <holger@eitzenberger.org>

--- filter/raw2packet/ulogd_raw2packet_BASE.c
+++ filter/raw2packet/ulogd_raw2packet_BASE.c
@@ -11,6 +11,9 @@
  *
  * (C) 2000-2005 by Harald Welte <laforge@gnumonks.org>
  *
+ * Put a step further by H. Eitzenberger <holger@eitzenberger.org>,
+ * Astaro AG 2008.
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 
  *  as published by the Free Software Foundation
@@ -23,7 +26,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 #include <ulogd/ulogd.h>
 #include <ulogd/common.h>
@@ -37,91 +39,122 @@
 #include <netinet/udp.h>
 
 
+enum OutKeys {
+	O_IpSAddr = 0,
+	O_IpDAddr,
+	O_IpProto,
+	O_IpTos,
+	O_IpTtl,
+	O_IpTotLen,
+	O_IpIhl,
+	O_IpCsum,
+	O_IpId,
+	O_IpFragOff,
+	O_TcpSPort,
+	O_TcpDPort,
+	O_TcpSeq,
+	O_TcpAckSeq,
+	O_TcpOff,
+	O_TcpReserved,
+	O_TcpWin,
+	O_TcpUrg,
+	O_TcpUrgp,
+	O_TcpAck,
+	O_TcpPsh,
+	O_TcpRst,
+	O_TcpSyn,
+	O_TcpFin,
+	O_TcpRes1,
+	O_TcpRes2,
+	O_TcpCsum,
+	O_UdpSPort,
+	O_UdpDPort,
+	O_UdpLen,
+	O_UdpCsum,
+	O_IcmpType,
+	O_IcmpCode,
+	O_IcmpEchoId,
+	O_IcmpEchoSeq,
+	O_IcmpGw,
+	O_IcmpFragMtu,
+	O_IcmpCsum,
+	O_AhEspSpi,
+};
+
 /***********************************************************************
  * 			IP HEADER
  ***********************************************************************/
-
-static struct ulogd_key iphdr_rets[] = {
-	{ 
+static struct ulogd_key out_keys[] = {
+	[O_IpSAddr] = {
 		.type = ULOGD_RET_IPADDR,
-		.flags = ULOGD_RETF_NONE, 
 		.name = "ip.saddr", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_sourceIPv4Address,
 		},
 	},
-	{
+	[O_IpDAddr] = {
 		.type = ULOGD_RET_IPADDR,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.daddr", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_destinationIPv4Address,
 		},
 	},
-	{
+	[O_IpProto] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.protocol", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_protocolIdentifier,
 		},
 	},
-	{
+	[O_IpTos] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.tos", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_classOfServiceIPv4,
 		},
 	},
-	{
+	[O_IpTtl] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.ttl", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_ipTimeToLive,
 		},
 	},
-	{
+	[O_IpTotLen] {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.totlen", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_totalLengthIPv4,
 		},
 	},
-	{
+	[O_IpIhl] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.ihl", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_internetHeaderLengthIPv4,
 		},
 	},
-	{
+	[O_IpCsum] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.csum", 
 	},
-	{
+	[O_IpId] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.id", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_identificationIPv4,
 		},
 	},
-	{
+	[O_IpFragOff] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ip.fragoff", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
@@ -130,202 +163,166 @@ static struct ulogd_key iphdr_rets[] = {
 	},
 
 	/* 10 */
-
-	{
+	[O_TcpSPort] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.sport", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_tcpSourcePort,
 		},
 	},
-	{
+	[O_TcpDPort] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.dport", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_tcpDestinationPort,
 		},
 	},
-	{
+	[O_TcpSeq] = {
 		.type = ULOGD_RET_UINT32,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.seq", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_tcpSequenceNumber,
 		},
 	},
-	{
+	[O_TcpAckSeq] = {
 		.type = ULOGD_RET_UINT32,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.ackseq", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_tcpAcknowledgementNumber,
 		},
 	},
-	{
+	[O_TcpOff] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE, 
 		.name = "tcp.offset",
 	},
-	{
+	[O_TcpReserved] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.reserved",
 	}, 
-	{
+	[O_TcpWin] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.window",
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_tcpWindowSize,
 		},
 	},
-	{
+	[O_TcpUrg] = {
 		.type = ULOGD_RET_BOOL, 
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.urg", 
 	},
-	{
+	[O_TcpUrgp] = {
 		.type = ULOGD_RET_UINT16, 
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.urgp",
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_tcpUrgentPointer,
 		},
 	},
-	{
+	[O_TcpAck] = {
 		.type = ULOGD_RET_BOOL, 
-		.flags = ULOGD_RETF_NONE, 
 		.name = "tcp.ack", 
 	},
-	{
+	[O_TcpPsh] = {
 		.type = ULOGD_RET_BOOL,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.psh",
 	},
-	{
+	[O_TcpRst] = {
 		.type = ULOGD_RET_BOOL,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.rst",
 	},
-	{
+	[O_TcpSyn] = {
 		.type = ULOGD_RET_BOOL,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.syn",
 	},
-	{
+	[O_TcpFin] = {
 		.type = ULOGD_RET_BOOL,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.fin",
 	},
-	{
+	[O_TcpRes1] = {
 		.type = ULOGD_RET_BOOL,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.res1",
 	},
-	{
+	[O_TcpRes2] = {
 		.type = ULOGD_RET_BOOL,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.res2",
 	},
-	{
+	[O_TcpCsum] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "tcp.csum",
 	},
 
 	/* 27 */
-
-	{
+	[O_UdpSPort] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "udp.sport", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF, 
 			.field_id = IPFIX_udpSourcePort,
 		},
 	},
-	{
+	[O_UdpDPort] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "udp.dport", 
 		.ipfix = { 
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_udpDestinationPort,
 		},
 	},
-	{
+	[O_UdpLen] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "udp.len", 
 	},
-	{
+	[O_UdpCsum] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "udp.csum",
 	},
 
 	/* 31 */
-
-
-	{
+	[O_IcmpType] = {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.type", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_icmpTypeIPv4,
 		},
 	},
-	{
+	[O_IcmpCode] {
 		.type = ULOGD_RET_UINT8,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.code", 
 		.ipfix = {
 			.vendor = IPFIX_VENDOR_IETF,
 			.field_id = IPFIX_icmpCodeIPv4,
 		},
 	},
-	{
+	[O_IcmpEchoId] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.echoid", 
 	},
-	{
+	[O_IcmpEchoSeq] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.echoseq",
 	},
-	{
+	[O_IcmpGw] = {
 		.type = ULOGD_RET_IPADDR,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.gateway", 
 	},
-	{
+	[O_IcmpFragMtu] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.fragmtu", 
 	},
-	{
+	[O_IcmpCsum] = {
 		.type = ULOGD_RET_UINT16,
-		.flags = ULOGD_RETF_NONE,
 		.name = "icmp.csum",
 	},
-	{
+	[O_AhEspSpi] = {
 		.type = ULOGD_RET_UINT32,
-		.flags = ULOGD_RETF_NONE,
 		.name = "ahesp.spi",
 	},
-
-	/* 39 */
-
 };
 
 static void *
@@ -341,30 +338,30 @@ ipv4_data(const struct iphdr *iph)
 static int
 _interp_tcp(const struct ulogd_pluginstance *pi, const struct iphdr *iph)
 {
-	struct ulogd_key *ret = &pi->output.keys[10];
+	struct ulogd_key *ret = pi->output.keys;
 	const struct tcphdr *tcph = ipv4_data(iph);
 
 	assert(iph->protocol == IPPROTO_TCP);
 
-	key_u16(&ret[0], ntohs(tcph->source));
-	key_u16(&ret[1], ntohs(tcph->dest));
-	key_u32(&ret[2], ntohl(tcph->seq));
-	key_u32(&ret[3], ntohl(tcph->ack_seq));
-	key_u8(&ret[4], ntohs(tcph->doff));
-	key_u8(&ret[5], ntohs(tcph->res1));
-	key_u16(&ret[6],ntohs(tcph->window));
+	key_u16(&ret[O_TcpSPort], ntohs(tcph->source));
+	key_u16(&ret[O_TcpDPort], ntohs(tcph->dest));
+	key_u32(&ret[O_TcpSeq], ntohl(tcph->seq));
+	key_u32(&ret[O_TcpAckSeq], ntohl(tcph->ack_seq));
+	key_u8(&ret[O_TcpOff], ntohs(tcph->doff));
+	key_u8(&ret[O_TcpReserved], ntohs(tcph->res1));
+	key_u16(&ret[O_TcpWin],ntohs(tcph->window));
 
-	key_bool(&ret[7], tcph->urg);
+	key_bool(&ret[O_TcpUrg], tcph->urg);
 	if (tcph->urg)
-		key_u16(&ret[8], ntohs(tcph->urg_ptr));
-	key_bool(&ret[9], tcph->ack);
-	key_bool(&ret[10],  tcph->psh);
-	key_bool(&ret[11], tcph->rst);
-	key_bool(&ret[12], tcph->syn);
-	key_bool(&ret[13], tcph->fin);
-	key_bool(&ret[14], tcph->res1);
-	key_bool(&ret[15], tcph->res2);
-	key_bool(&ret[16], ntohs(tcph->check));
+		key_u16(&ret[O_TcpUrgp], ntohs(tcph->urg_ptr));
+	key_bool(&ret[O_TcpAck], tcph->ack);
+	key_bool(&ret[O_TcpPsh],  tcph->psh);
+	key_bool(&ret[O_TcpRst], tcph->rst);
+	key_bool(&ret[O_TcpSyn], tcph->syn);
+	key_bool(&ret[O_TcpFin], tcph->fin);
+	key_bool(&ret[O_TcpRes1], tcph->res1);
+	key_bool(&ret[O_TcpRes2], tcph->res2);
+	key_bool(&ret[O_TcpCsum], ntohs(tcph->check));
 	
 	return 0;
 }
@@ -376,15 +373,15 @@ _interp_tcp(const struct ulogd_pluginstance *pi, const struct iphdr *iph)
 static int
 _interp_udp(const struct ulogd_pluginstance *pi, const struct iphdr *iph)
 {
-	struct ulogd_key *ret = &pi->output.keys[27];
+	struct ulogd_key *ret = pi->output.keys;
 	const struct udphdr *udph = ipv4_data(iph);
 
 	assert(iph->protocol == IPPROTO_UDP);
 
-	key_u16(&ret[0], ntohs(udph->source));
-	key_u16(&ret[1], ntohs(udph->dest));
-	key_u16(&ret[2], ntohs(udph->len));
-	key_u16(&ret[3], ntohs(udph->check));
+	key_u16(&ret[O_UdpSPort], ntohs(udph->source));
+	key_u16(&ret[O_UdpDPort], ntohs(udph->dest));
+	key_u16(&ret[O_UdpLen], ntohs(udph->len));
+	key_u16(&ret[O_UdpCsum], ntohs(udph->check));
 	
 	return 0;
 }
@@ -396,33 +393,33 @@ _interp_udp(const struct ulogd_pluginstance *pi, const struct iphdr *iph)
 static int
 _interp_icmp(const struct ulogd_pluginstance *pi, const struct iphdr *iph)
 {
-	struct ulogd_key *ret = &pi->output.keys[31];
+	struct ulogd_key *ret = pi->output.keys;
 	struct icmphdr *icmph = ipv4_data(iph);
 
 	assert(iph->protocol == IPPROTO_ICMP);
 	
-	key_u8(&ret[0], icmph->type);
-	key_u8(&ret[1], icmph->code);
+	key_u8(&ret[O_IcmpType], icmph->type);
+	key_u8(&ret[O_IcmpCode], icmph->code);
 
 	switch (icmph->type) {
 	case ICMP_ECHO:
 	case ICMP_ECHOREPLY:
-		key_u16(&ret[2], ntohs(icmph->un.echo.id));
-		key_u16(&ret[3], ntohs(icmph->un.echo.sequence));
+		key_u16(&ret[O_IcmpEchoId], ntohs(icmph->un.echo.id));
+		key_u16(&ret[O_IcmpEchoSeq], ntohs(icmph->un.echo.sequence));
 		break;
 
 	case ICMP_REDIRECT:
 	case ICMP_PARAMETERPROB:
-		key_u32(&ret[4], ntohl(icmph->un.gateway));
+		key_u32(&ret[O_IcmpGw], ntohl(icmph->un.gateway));
 		break;
 
 	case ICMP_DEST_UNREACH:
 		if (icmph->code == ICMP_FRAG_NEEDED)
-			key_u16(&ret[5], ntohs(icmph->un.frag.mtu));
+			key_u16(&ret[O_IcmpFragMtu], ntohs(icmph->un.frag.mtu));
 		break;
 	}
 
-	key_u16(&ret[6], icmph->checksum);
+	key_u16(&ret[O_IcmpCsum], icmph->checksum);
 
 	return 0;
 }
@@ -488,7 +485,7 @@ _interp_iphdr(struct ulogd_pluginstance *pi)
 	return 0;
 }
 
-static struct ulogd_key base_inp[] = {
+static struct ulogd_key in_keys[] = {
 	{ 
 		.type = ULOGD_RET_RAW,
 		.name = "raw.pkt", 
@@ -502,13 +499,13 @@ static struct ulogd_key base_inp[] = {
 static struct ulogd_plugin base_plugin = {
 	.name = "BASE",
 	.input = {
-		.keys = base_inp,
-		.num_keys = ARRAY_SIZE(base_inp),
+		.keys = in_keys,
+		.num_keys = ARRAY_SIZE(in_keys),
 		.type = ULOGD_DTYPE_RAW,
 		},
 	.output = {
-		.keys = iphdr_rets,
-		.num_keys = ARRAY_SIZE(iphdr_rets),
+		.keys = out_keys,
+		.num_keys = ARRAY_SIZE(out_keys),
 		.type = ULOGD_DTYPE_PACKET,
 		},
 	.interp = &_interp_iphdr,
