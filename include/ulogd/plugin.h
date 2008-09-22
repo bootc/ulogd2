4ef340582e49dc1a28707bc2711875f885c417ce
Holger Eitzenberger <holger@eitzenberger.org>
2008-09-22 13:59:49 +0200

    Move stack_resolve_keys() to plugin code
    Signed-off-by: Holger Eitzenberger <holger@eitzenberger.org>

--- include/ulogd/plugin.h
+++ include/ulogd/plugin.h
@@ -84,6 +84,5 @@ struct ulogd_key *ulogd_key_find(const struct ulogd_keyset *,
 int ulogd_plugin_init(void);
 int stack_fsm(struct ulogd_pluginstance_stack *);
 int stack_reconfigure(struct ulogd_pluginstance_stack *);
-int stack_resolve_keys(const struct ulogd_pluginstance_stack *);
 
 #endif /* PLUGIN_H */
