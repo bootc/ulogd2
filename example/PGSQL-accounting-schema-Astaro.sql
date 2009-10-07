163703a50db5ef885fb66c2778fa26a47a5a161e
Holger Eitzenberger <heitzenberger@astaro.com>
2009-10-07 17:06:30 +0200

    PGSQL: do not use coalesce feature in Astaro example
    Signed-off-by: Holger Eitzenberger <holger@eitzenberger.org>

--- example/PGSQL-accounting-schema-Astaro.sql
+++ example/PGSQL-accounting-schema-Astaro.sql
@@ -27,19 +27,14 @@ create index accounting_idx on accounting (
 );
 
 create or replace function ins_accounting(
-	saddr inet, saddr6 inet, daddr inet, daddr6 inet,
-	proto integer, port integer,
+	saddr inet, daddr inet, proto integer, port integer,
 	in_len bigint, in_cnt bigint, out_len bigint, out_cnt bigint,
 	flow_start integer, duration integer
 ) returns void as $$
 declare
 	day		date;
-	src		inet;
-	dst		inet;
 begin
 	day = date 'epoch' + flow_start * interval '1 second';
-	src = coalesce(saddr, saddr6);
-	dst = coalesce(daddr, daddr6);
 
 	update accounting set
 		raw_in_pktlen = raw_in_pktlen + in_len,
@@ -49,8 +44,8 @@ begin
 		flow_count = flow_count + 1
 	where
 		logday = day and
-		srcip = src and
-		dstip = dst and
+		srcip = saddr and
+		dstip = daddr and
 		ip_protocol = proto and
 		l4_dport = port;
 
@@ -60,7 +55,7 @@ begin
 			raw_in_pktlen, raw_in_pktcount, raw_out_pktlen, raw_out_pktcount,
 			logday, flow_duration, flow_count
 		) values (
-			src, dst, proto, port, in_len, in_cnt, out_len, out_cnt,
+			saddr, daddr, proto, port, in_len, in_cnt, out_len, out_cnt,
 			day, duration, 1
 		);
 	end if;
